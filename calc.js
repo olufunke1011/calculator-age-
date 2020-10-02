f55b7467bd7346df28479f6571a01e50b6f7d0ca
olufunke owoseni <owoseniolufunke1011@gmail.com>
2020-10-02 12:56:41 +0100

    continuation

--- calc.js
+++ calc.js
@@ -1,4 +1,7 @@
 const button1 = document.querySelector('.click')
 const secondPage = document.querySelector('.cont2')
+const firstPage = document.querySelector('.blueBGD')
 
-button1.addEventListener()
+button1.addEventListener('click', function(){
+    secondPage.classList.remove('hidden');
+})
