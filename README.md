9065589ffa04431bb93b0b0f3763dae53a5815a6
olufunke owoseni <owoseniolufunke1011@gmail.com>
2020-10-02 14:55:44 +0100

    final adding calc.css

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # calculator-age-
 It helps calculate age in days and years
+
+## Live Demo Link
+https://raw.githack.com/olufunke1011/calculator-age-/main/calc.html
--- calc.js
+++ calc.js
@@ -1,7 +1,36 @@
 const button1 = document.querySelector('.click')
 const secondPage = document.querySelector('.cont2')
 const firstPage = document.querySelector('.blueBGD')
+const submitButt = document.querySelector('.blockB');
+const thirdPage = document.querySelector ('.prompt');
+
+const hey = document.querySelector ('.hey')
+const showName = document.querySelector ('.showName')
+const days = document.querySelector ('.days')
 
 button1.addEventListener('click', function(){
     secondPage.classList.remove('hidden');
+    firstPage.classList.add('hidden');
+})
+
+submitButt.addEventListener ('click', function(e){
+    e.preventDefault();
+
+var yourName = document.getElementById("name").value;
+var yourAge = document.getElementById("age").value;
+var ageInDays = yourAge * 365;
+
+if (yourName == " " || yourAge == "") {
+    alert("input missing field");
+  
+}
+else{
+    hey.textContent = "HEY THERE !!!" ;
+    showName.textContent = "YOUR NAME IS: " + yourName;
+    days.textContent = "YOU HAVE LIVED " + ageInDays + " DAYS ON EARTH"
+}
+
+secondPage.classList.add('hidden');
+thirdPage.classList.remove('hidden');
+
 })
